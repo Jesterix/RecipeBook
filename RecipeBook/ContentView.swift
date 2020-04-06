080988eaa1d230d07756f7f283e9bf2587c69a83
Georgy Khaydenko <gh@van.work>
2020-04-06 15:19:08 +0300

    Initial Commit

---
+++ RecipeBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeBook
+//
+//  Created by Георгий Хайденко on 06.04.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
