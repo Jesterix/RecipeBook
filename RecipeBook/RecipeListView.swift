3edee788521b6d8221da6f9a3cf4bbfbeca71bcd
Georgy Khaydenko <gh@van.work>
2020-04-10 17:47:35 +0300

    added textField for new recipe adding

--- RecipeBook/RecipeListView.swift
+++ RecipeBook/RecipeListView.swift
@@ -10,32 +10,44 @@ import SwiftUI
 
 struct RecipeListView: View {
     @EnvironmentObject var recipeListViewModel: RecipeListViewModel
+    @State var titleForRecipe: String = ""
 
     var body: some View {
-                List {
-            ForEach(recipeListViewModel.recipes) { recipe in
-                HStack {
-                    Text(recipe.title)
-                    Spacer()
-                    VStack {
-                        HStack {
-                            Spacer()
-                            Text (recipe.ingredients[0].title)
-                        }
-                        HStack {
-                            Spacer()
-                            recipe.ingredients[0].quantity.map
-                                { Text (String($0)) }
-                            recipe.ingredients[0].measurement.map
-                                { Text ($0.title) }
-                        }
-                        HStack {
-                            Spacer()
-                            Text (recipe.text ?? "no text")
+        VStack {
+            List {
+                ForEach(recipeListViewModel.recipes) { recipe in
+                    HStack {
+                        Text(recipe.title)
+                        Spacer()
+                        VStack {
+                            HStack {
+                                Spacer()
+                                Text (recipe.ingredients[0].title)
+                            }
+                            HStack {
+                                Spacer()
+                                recipe.ingredients[0].quantity.map
+                                    { Text (String($0)) }
+                                recipe.ingredients[0].measurement.map
+                                    { Text ($0.title) }
+                            }
+                            HStack {
+                                Spacer()
+                                Text (recipe.text ?? "no text")
+                            }
                         }
                     }
                 }
             }
+            Spacer()
+
+            TextField("Enter title for recipe...", text: $titleForRecipe, onEditingChanged: { (changed) in
+                print("Username onEditingChanged - \(changed)")
+            }) {
+                print("Username onCommit")
+            }
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
         }
     }
 }
